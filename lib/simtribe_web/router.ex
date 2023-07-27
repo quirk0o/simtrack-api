d7b7dabb04fdbe1b774b160f5f20a9aed125ec79
Beata Obrok <beata.obrok@u2i.com>
2023-07-27 14:05:23 +0200

    feat: add user accounts

--- lib/simtribe_web/router.ex
+++ lib/simtribe_web/router.ex
@@ -1,6 +1,8 @@
 defmodule SimTribeWeb.Router do
   use SimTribeWeb, :router
 
+  import SimTribeWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,10 +10,12 @@ defmodule SimTribeWeb.Router do
     plug :put_root_layout, html: {SimTribeWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug :fetch_current_user
   end
 
   scope "/", SimTribeWeb do
@@ -39,6 +43,44 @@ defmodule SimTribeWeb.Router do
 
       live_dashboard "/dashboard", metrics: SimTribeWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", SimTribeWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{SimTribeWeb.UserAuthLive, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", SimTribeWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{SimTribeWeb.UserAuthLive, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", SimTribeWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{SimTribeWeb.UserAuthLive, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
